35f18ce4a2746ef44f64c377be230f80eb1613d2
J. Benjamin Leeds <ben@benjaminleeds.com>
2020-11-24 15:13:59 -0800

    add form submit upon completion functionality

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class SignUpApp extends StatelessWidget {
     return MaterialApp(
       routes: {
         '/': (context) => SignUpScreen(),
+        '/welcome': (context) => WelcomeScreen(),
       },
     );
   }
@@ -30,6 +31,17 @@ class SignUpScreen extends StatelessWidget {
   }
 }
 
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
+      ),
+    );
+  }
+}
+
 class SignUpForm extends StatefulWidget {
   @override
   _SignUpFormState createState() => _SignUpFormState();
@@ -40,11 +52,36 @@ class _SignUpFormState extends State<SignUpForm> {
   final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
 
-  double _formProgress = 0;
+  double _formProgress = 0; // stores the form completion state (0-1)
+
+  void _showWelcomeScreen(){
+    Navigator.of(context).pushNamed('/welcome'); // route to welcome screen
+  }
+
+  void _updateFormProgress(){
+    var progress = 0.0; // a double
+    var controllers = [ // what is this type, just an array?
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+
+    // check each field, add the fraction that one completed field represents
+    for (var controller in controllers){
+      if (controller.value.text.isNotEmpty){
+        progress += 1 / controllers.length; 
+      }
+    }
+
+    setState(() => _formProgress = progress);
+    print("The form progress is: " + _formProgress.toString());
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -83,7 +120,8 @@ class _SignUpFormState extends State<SignUpForm> {
                 return states.contains(MaterialState.disabled) ? null : Colors.blue;
               }),
             ),
-            onPressed: null,
+            // enable form submit only when form is complete w/ ternary operator
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: Text('Sign up'),
           ),
         ],
